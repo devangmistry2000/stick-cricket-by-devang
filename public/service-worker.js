4fb32a2a3f2806c3133e35607f0ce72e3416667b
devangmistry2000 <devang1.mistry@gmail.com>
2026-07-12 10:43:14 +0530

    Refresh cache for illustrated character release

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,28 @@
-const CACHE = 'cricket-captain-3d-v1';
-const CORE = ['./', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+const CACHE = 'cricket-captain-illustrated-v2';
+const CORE = [
+  './',
+  './manifest.webmanifest',
+  './characters.html',
+  './assets/characters/batter-idle.svg',
+  './assets/characters/batter-shot.svg',
+  './assets/characters/batter-miss.svg',
+  './assets/characters/bowler-idle.svg',
+  './assets/characters/bowler-delivery.svg',
+  './assets/characters/keeper.svg',
+  './assets/characters/fielder.svg'
+];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match('./'))));
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./'))));
 });
